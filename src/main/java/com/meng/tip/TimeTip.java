30f2a09b5a686c33603b00ad3efc12d13235f088
swordarrow2 <2856986197@qq.com>
2018-11-23 18:10:26 +0800

    添加了圣宙五放学提醒

--- src/main/java/com/meng/tip/TimeTip.java
+++ src/main/java/com/meng/tip/TimeTip.java
@@ -5,18 +5,17 @@ import java.util.Calendar;
 import com.meng.Autoreply;
 
 public class TimeTip extends Thread {
-	// 每两个小时内最速说第一句话时催更
-	long groupZuisu = 855927922L;
-	long groupZiyuan = 857548607L;
-	long groupDNFmoDao = 424838564L;
-	long groupXueXi = 312342896L;
-	long ziyuan = 2198634315L;
-	long jizhe = 1012539034L;
-	long diuren = 1581137837L;
-	boolean tipedZuisu = true;
-	boolean tipedZiyuan = true;
-	boolean tipedModao = true;
-	boolean tipedDiuRen = false;
+	private long groupZuisu = 855927922L;
+	private long groupZiyuan = 857548607L;
+	private long groupDNFmoDao = 424838564L;
+	private long groupXueXi = 312342896L;
+	private long groupTaiZhang = 859561731L;
+	private long ziyuan = 2198634315L;
+	private long jizhe = 1012539034L;
+	private long diuren = 1581137837L;
+	private boolean tipedZuisu = true;
+	private boolean tipedZiyuan = true;
+	private boolean tipedDiuRen = false;
 
 	public TimeTip() {
 	}
@@ -33,16 +32,24 @@ public class TimeTip extends Thread {
 				if (c.get(Calendar.HOUR_OF_DAY) % 2 == 0) {
 					tipedZuisu = false;// 每两个小时重置提醒标记
 				}
-				if (c.getActualMaximum(Calendar.DAY_OF_MONTH) == c.get(Calendar.DATE)
-						&& (c.get(Calendar.HOUR_OF_DAY) == 12 || c.get(Calendar.HOUR_OF_DAY) == 16
-								|| c.get(Calendar.HOUR_OF_DAY) == 22)) {
-					Autoreply.sendMessage(groupDNFmoDao, 0, "最后一天莉，，，看看冒险团商店");
-					Autoreply.sendMessage(groupXueXi, 0, "最后一天莉，，，看看冒险团商店");
-				}
-				if (c.get(Calendar.DAY_OF_WEEK) == 4 && (c.get(Calendar.HOUR_OF_DAY) == 12
-						|| c.get(Calendar.HOUR_OF_DAY) == 16 || c.get(Calendar.HOUR_OF_DAY) == 22)) {
-					Autoreply.sendMessage(groupDNFmoDao, 0, "星期三莉，，，看看成长胶囊");
-					Autoreply.sendMessage(groupXueXi, 0, "星期三莉，，，看看成长胶囊");
+
+				if (getTipHour(c)) {
+					if (c.getActualMaximum(Calendar.DAY_OF_MONTH) == c.get(Calendar.DATE)) {
+						Autoreply.sendMessage(groupDNFmoDao, 0, "最后一天莉，，，看看冒险团商店");
+						Autoreply.sendMessage(groupXueXi, 0, "最后一天莉，，，看看冒险团商店");
+					}
+					switch (c.get(Calendar.DAY_OF_WEEK)) {
+					case 4:
+						Autoreply.sendMessage(groupDNFmoDao, 0, "星期三莉，，，看看成长胶囊");
+						Autoreply.sendMessage(groupXueXi, 0, "星期三莉，，，看看成长胶囊");
+						break;
+					case 5:
+						Autoreply.sendMessage(groupTaiZhang, 0, "注意，明天星期五");
+						break;
+					case 6:
+						Autoreply.sendMessage(groupTaiZhang, 0, "注意，今天星期五");
+						break;
+					}
 				}
 			}
 			try {
@@ -51,6 +58,11 @@ public class TimeTip extends Thread {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	private boolean getTipHour(Calendar c) {
+		return (c.get(Calendar.HOUR_OF_DAY) == 12 || c.get(Calendar.HOUR_OF_DAY) == 16
+				|| c.get(Calendar.HOUR_OF_DAY) == 22);
 	}
 
 	public boolean check(long fromGroup, long fromQQ) {
