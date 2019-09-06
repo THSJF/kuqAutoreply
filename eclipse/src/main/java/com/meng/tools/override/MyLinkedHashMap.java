34f22f862ece6cca2d38d156ca1a7e3bfb1b1d15
swordarrow2 <2856986197@qq.com>
2019-09-07 00:19:47 +0800

    新的toString

--- eclipse/src/main/java/com/meng/tools/override/MyLinkedHashMap.java
+++ eclipse/src/main/java/com/meng/tools/override/MyLinkedHashMap.java
@@ -5,11 +5,21 @@ public class MyLinkedHashMap<K,V> extends LinkedHashMap {
 
 	@Override
 	public String toString() {
-		StringBuilder sb=new StringBuilder();
-		for (Entry entry:entrySet()) {
-			sb.append(entry.getKey().toString()).append(" ").append(entry.getValue().toString()).append("\n");
+        Set<Map.Entry<K, V>> keyset = this.entrySet();
+        Iterator<Map.Entry<K, V>> i = keyset.iterator();
+        if (!i.hasNext()) {
+			return "";
 		  }
-		return sb.toString();
+        StringBuffer buffer = new StringBuffer();
+        while (true) {
+            Map.Entry<K, V> me = i.next();
+            K key = me.getKey();
+            V value = me.getValue();
+            buffer.append(key.toString() + " ");
+            buffer.append(value.toString() + "\n");
+            if (!i.hasNext()) {
+				return buffer.toString();
+			  }
+		  }
 	  }
-
   }
