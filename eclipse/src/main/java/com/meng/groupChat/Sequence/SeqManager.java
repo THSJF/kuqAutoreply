d3f94ee81544c9c2f33e816f3d0e5d99a1d51688
swordarrow2 <2856986197@qq.com>
2019-09-08 09:50:59 +0800

    从json读取

--- eclipse/src/main/java/com/meng/groupChat/Sequence/SeqManager.java
+++ eclipse/src/main/java/com/meng/groupChat/Sequence/SeqManager.java
@@ -1,15 +1,44 @@
 package com.meng.groupChat.Sequence;
-import java.util.*;
+import com.google.gson.*;
+import com.google.gson.internal.*;
 import com.meng.*;
+import com.meng.tools.*;
+import java.io.*;
+import java.nio.charset.*;
+import java.util.*;
 
 public class SeqManager {
-	public String[] time=new String[]{"苟","利","国","家","生","死","以","岂","因","祸","福","避","趋","之"};
-	public String[] menger=new String[]{"此","生","无","悔","入","东","方","来","世","愿","生","幻","想","乡"};
+	//public String[] time=new String[]{"苟","利","国","家","生","死","以","岂","因","祸","福","避","趋","之"};
+	//public String[] menger=new String[]{"此","生","无","悔","入","东","方","来","世","愿","生","幻","想","乡"};
 	private ArrayList<SeqBean> seqs=new ArrayList<>();
-
+	public JsonObject jsonObject;
 	public SeqManager() {
-		seqs.add(new SeqBean(time, 1));
-		seqs.add(new SeqBean(menger, 2));
+		File liveTimeFile = new File(Autoreply.appDirectory + "seq.json");
+        if (!liveTimeFile.exists()) {
+            saveLiveTime();
+		  }
+        jsonObject = new JsonParser().parse(Methods.readFileToString(liveTimeFile.getAbsolutePath())).getAsJsonObject();
+		LinkedTreeMap linkedTreeMap = (LinkedTreeMap) ((Object)jsonObject);
+		for (Object o : linkedTreeMap.keySet()) {
+			String key = (String) o;
+			JsonArray jsonArray=(JsonArray) linkedTreeMap.get(key);
+			String[] content=new String[jsonArray.size()];
+			for (int i=0;i < content.length;++i) {
+				content[i] = jsonArray.get(i).getAsString();
+			  }
+			int flag=0;
+			switch (key) {
+				case "time":
+				case "time1":
+				  flag = 1;
+				  break;
+				case "menger":
+				case "menger2":
+				  flag = 2;
+				  break;
+			  }
+			seqs.add(new SeqBean(content, flag));
+		  }
 	  }
 
 	public boolean check(long fromGroup, long fromQQ, String msg) {
@@ -36,5 +65,18 @@ public class SeqManager {
 			  }
 		  }
 		return false;
+	  }
+
+	private void saveLiveTime() {
+        try {
+            File file = new File(Autoreply.appDirectory + "seq.json");
+            FileOutputStream fos = new FileOutputStream(file);
+            OutputStreamWriter writer = new OutputStreamWriter(fos, StandardCharsets.UTF_8);
+            writer.write(new Gson().toJson(new Object()));
+            writer.flush();
+            fos.close();
+		  } catch (IOException e) {
+            e.printStackTrace();
+		  }
 	  }
   }
