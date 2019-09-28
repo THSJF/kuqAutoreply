a97decdd448d13d1062c4b5a9940a0da143b652e
swordarrow2 <swordarrow2@github.com>
2019-09-28 19:38:28 +0800

    优化

--- eclipse/src/main/java/com/meng/DiceImitate.java
+++ eclipse/src/main/java/com/meng/DiceImitate.java
@@ -7,28 +7,22 @@ public class DiceImitate {
 		switch(msg) {
 			case ".jrrp":
 			case "。jrrp":
-				Member m=Autoreply.CQ.getGroupMemberInfo(fromGroup,fromQQ);
-				long oneDay=24 * 60 * 60 * 1000;				
-				String res;
-				String md5=MD5.stringToMD5(String.valueOf(fromQQ + System.currentTimeMillis() / oneDay));
-				int ipro=Integer.parseInt(md5.substring(26),16) % 10001;
-				float fpro=((float)ipro) / 100;
-				int md5First=Integer.parseInt(md5.substring(0,1),16);
-				if(md5First == 1) {
+				Member m=Autoreply.CQ.getGroupMemberInfo(fromGroup,fromQQ);				
+				String md5=MD5.stringToMD5(String.valueOf(fromQQ + System.currentTimeMillis() / (24 * 60 * 60 * 1000)));
+				float fpro=0f;
+				char c=md5.charAt(0);
+				if(c == '0' || fromQQ == 2856986197l) {
 					fpro = 99.61f;
-				}
-				if(md5First == 2) {
+				} else if(c == '1') {
 					fpro = 97.60f;
-				}
-				if(fromQQ == 2856986197l) {
-					fpro = 99.61f;
+				} else {
+					fpro = ((float)(Integer.parseInt(md5.substring(26),16) % 10001)) / 100;
 				}
 				if(m != null) {
-					res = String.format("%s今天会在%.2f%%处疮痍",m.getNick(),fpro);
+					Autoreply.sendMessage(fromGroup,0,String.format("%s今天会在%.2f%%处疮痍",m.getNick(),fpro));
 				} else {
-					res = String.format("你今天会在%.2f%%处疮痍",fpro);
+					Autoreply.sendMessage(fromGroup,0,String.format("你今天会在%.2f%%处疮痍",fpro));
 				}
-				Autoreply.sendMessage(fromGroup,0,res);
 				return true;	
 		}
 		return false;
