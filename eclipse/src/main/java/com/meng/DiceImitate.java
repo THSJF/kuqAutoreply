fa35bdf96f946b80cd42123b6f5aa8dd4226624d
swordarrow2 <2856986197@qq.com>
2019-10-04 13:19:18 +0800

    game

--- eclipse/src/main/java/com/meng/DiceImitate.java
+++ eclipse/src/main/java/com/meng/DiceImitate.java
@@ -10,6 +10,27 @@ public class DiceImitate {
 	private String[] music;
 	private String[] name;
 
+	private String[] pl02 = new String[]{"范围重视型", "高灵击伤害 平衡型", "威力重视型"};
+    private String[] pl03 = new String[]{"博丽灵梦", "魅魔", "雾雨魔理沙", "爱莲", "小兔姬", "卡娜·安娜贝拉尔", "朝仓理香子", "北白河千百合", "冈崎梦美"};
+    private String[] pl04 = new String[]{"博丽灵梦 诱导", "博丽灵梦 大范围", "雾雨魔理沙 激光", "雾雨魔理沙 高速射击"};
+    private String[] pl05 = new String[]{"博丽灵梦", "雾雨魔理沙", "魅魔", "幽香"};
+    private String[] pl06 = new String[]{"博丽灵梦 灵符", "博丽灵梦 梦符", "雾雨魔理沙 魔符", "雾雨魔理沙 恋符"};
+    private String[] pl07 = new String[]{"博丽灵梦 灵符", "博丽灵梦 梦符", "雾雨魔理沙 魔符", "雾雨魔理沙 恋符", "十六夜咲夜 幻符", "十六夜咲夜 时符"};
+    private String[] pl08 = new String[]{"幻想的结界组", "咏唱禁咒组", "梦幻的红魔组", "幽冥之住人组", "博丽灵梦", "八云紫", "雾雨魔理沙", "爱丽丝·玛格特罗依德", "蕾米莉亚·斯卡蕾特", "十六夜咲夜", "西行寺幽幽子", "魂魄妖梦"};
+    private String[] pl09 = new String[]{"博丽灵梦", "雾雨魔理沙", "十六夜咲夜", "魂魄妖梦", "铃仙·优昙华院·因幡", "琪露诺", "莉莉卡·普莉兹姆利巴", "梅露兰·普莉兹姆利巴", "露娜萨·普莉兹姆利巴", "米斯蒂娅·萝蕾拉", "因幡帝", "射命丸文", "梅蒂欣·梅兰可莉", "风见幽香", "小野冢小町", "四季映姬·亚玛萨那度"};
+    private String[] pl10 = new String[]{"博丽灵梦 诱导装备", "博丽灵梦 前方集中装备", "博丽灵梦 封印装备", "雾雨魔理沙 高威力装备", "雾雨魔理沙 贯通装备", "雾雨魔理沙 魔法使装备"};
+    private String[] pl11 = new String[]{"博丽灵梦 八云紫", "博丽灵梦 伊吹萃香", "博丽灵梦 射命丸文", "雾雨魔理沙 爱丽丝·玛格特罗依德", "雾雨魔理沙 帕秋莉", "雾雨魔理沙 河城荷取"};
+    private String[] pl12 = new String[]{"博丽灵梦 诱导型", "博丽灵梦 威力重视型", "雾雨魔理沙 贯通型", "雾雨魔理沙 范围重视型", "东风谷早苗 诱导型", "东风谷早苗 广范围炸裂型"};
+    private String[] pl13 = new String[]{"博丽灵梦", "雾雨魔理沙", "东风谷早苗", "魂魄妖梦"};
+    private String[] pl14 = new String[]{"博丽灵梦", "雾雨魔理沙", "十六夜咲夜"};
+    private String[] pl14s = new String[]{"使用妖器", "不使用妖器"};
+    private String[] pl15 = new String[]{"博丽灵梦", "雾雨魔理沙", "东风谷早苗", "铃仙·优昙华院·因幡"};
+    private String[] pl16 = new String[]{"博丽灵梦", "琪露诺", "射命丸文", "雾雨魔理沙"};
+    private String[] pl16s = new String[]{"春", "夏", "秋", "冬"};
+    private String[] pl17 = new String[]{"博丽灵梦", "雾雨魔理沙", "魂魄妖梦"};
+    private String[] pl17s = new String[]{"[CQ:emoji,id=128059]哥", "[CQ:emoji,id=128037]哥", "[CQ:emoji,id=128054]哥"};
+
+
 	public DiceImitate() {
 		spells = new String[]{
 			//th6
@@ -286,7 +307,7 @@ public class DiceImitate {
 				Autoreply.sendMessage(fromGroup, 0, String.format("%s今天会在%.2f%%处疮痍", pname, fpro));
 				return true;	
 			case "。jrrp":
-				Autoreply.sendMessage(fromGroup, 0, String.format("%s今天会在%s疮痍", pname, spells[md5Random(fromQQ) %  spells.length]));
+				Autoreply.sendMessage(fromGroup, 0, String.format("%s今天会在%s疮痍", pname, md5RanStr(fromQQ, spells)));
 		   		return true;
 		}
 
@@ -294,16 +315,16 @@ public class DiceImitate {
 			String drawcmd=msg.substring(6);
 			switch (drawcmd) {
 				case "help":
-					Autoreply.sendMessage(fromGroup, 0, "当前有:spell neta music grandma");
+					Autoreply.sendMessage(fromGroup, 0, "当前有:spell neta music grandma game");
 					return true;
 				case "spell":
 					Autoreply.sendMessage(fromGroup, 0, spells[new Random().nextInt(spells.length)]);
 					return true;
 				case "neta":
-					Autoreply.sendMessage(fromGroup, 0, String.format("%s今天宜打%s", pname, neta[md5Random(fromQQ) % neta.length]));
+					Autoreply.sendMessage(fromGroup, 0, String.format("%s今天宜打%s", pname, md5RanStr(fromQQ, neta)));
 					return true;
 				case "music":
-					Autoreply.sendMessage(fromGroup, 0, String.format("%s今天宜听%s", pname, music[md5Random(fromQQ) % music.length]));
+					Autoreply.sendMessage(fromGroup, 0, String.format("%s今天宜听%s", pname, md5RanStr(fromQQ, music)));
 					return true;
 				case "grandma":
 					String grandmaMd5=MD5.stringToMD5(String.valueOf(fromQQ + System.currentTimeMillis() / (24 * 60 * 60 * 1000)));
@@ -311,7 +332,84 @@ public class DiceImitate {
 						Autoreply.sendMessage(fromGroup, 0, String.format("%s今天宜认八云紫当奶奶", pname));
 						return true;
 					}
-					Autoreply.sendMessage(fromGroup, 0, String.format("%s今天宜认%s当奶奶", pname, name[md5Random(fromQQ) % name.length]));
+					Autoreply.sendMessage(fromGroup, 0, String.format("%s今天宜认%s当奶奶", pname, md5RanStr(fromQQ, name)));
+					return true;
+				case "game":
+					int gameNo=md5Random(fromQQ) % 15 + 2;
+					String gameName = null;
+					String charaName = null;
+					switch (gameNo) {
+						case 2:
+							gameName = "封魔录";
+							charaName = md5RanStr(fromQQ, pl02);
+							break;
+						case 3:
+							gameName = "梦时空";
+							charaName = md5RanStr(fromQQ, pl03);
+							break;
+						case 4:
+							gameName = "幻想乡";
+							charaName = md5RanStr(fromQQ, pl04);
+							break;
+						case 5:
+							gameName = "怪绮谈";
+							charaName = md5RanStr(fromQQ, pl05);
+							break;
+						case 6:
+							gameName = "红魔乡";
+							charaName = md5RanStr(fromQQ, pl06);
+							break;
+						case 7:
+							gameName = "妖妖梦";
+							charaName = md5RanStr(fromQQ, pl07);
+							break;
+						case 8:
+							gameName = "永夜抄";
+							charaName = md5RanStr(fromQQ, pl08);
+							break;
+						case 9:
+							gameName = "花映冢";
+							charaName = md5RanStr(fromQQ, pl09);
+							break;
+						case 10:
+							gameName = "风神录";
+							charaName = md5RanStr(fromQQ, pl10);
+							break;
+						case 11:
+							gameName = "地灵殿";
+							charaName = md5RanStr(fromQQ, pl11);
+							break;
+						case 12:
+							gameName = "星莲船";
+							charaName = md5RanStr(fromQQ, pl12);
+							break;
+						case 13:
+							gameName = "神灵庙";
+							charaName = md5RanStr(fromQQ, pl13);
+							break;
+						case 14:
+							gameName = "辉针城";
+							charaName = md5RanStr(fromQQ, pl14);
+							Autoreply.sendMessage(fromGroup, 0, String.format("%s今天宜用%s %s打%s", pname, charaName, md5RanStr(fromQQ + 1, pl14s), gameName));
+							return true;
+						case 15:
+							gameName = "绀珠传";
+							charaName = md5RanStr(fromQQ, pl15);
+							break;
+						case 16:
+							gameName = "天空璋";
+							charaName = md5RanStr(fromQQ, pl16);
+							Autoreply.sendMessage(fromGroup, 0, String.format("%s今天宜用%s %s打%s", pname, charaName, md5RanStr(fromQQ + 1, pl16s), gameName));
+							return true;
+						case 17:
+							gameName = "鬼形兽";
+							charaName = md5RanStr(fromQQ, pl17);
+							Autoreply.sendMessage(fromGroup, 0, String.format("%s今天宜用%s %s打%s", pname, charaName, md5RanStr(fromQQ + 1, pl17s), gameName));		
+							return true;
+						default:
+							return true;
+					}
+					Autoreply.sendMessage(fromGroup, 0, String.format("%s今天宜用%s打%s", pname, charaName, gameName));
 					return true;
 				default:
 					Autoreply.sendMessage(fromGroup, 0, "可用.draw help查看帮助");
@@ -325,6 +423,10 @@ public class DiceImitate {
 	private int md5Random(long fromQQ) {
 		String md5=MD5.stringToMD5(String.valueOf(fromQQ + System.currentTimeMillis() / (24 * 60 * 60 * 1000)));
 		return Integer.parseInt(md5.substring(26), 16);
+	}
+
+	private String md5RanStr(long fromQQ, String[] arr) {
+		return arr[md5Random(fromQQ)];
 	}
 
 	private String getName(long fromGroup, long fromQQ) {
