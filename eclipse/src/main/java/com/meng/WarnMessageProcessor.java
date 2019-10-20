cf1ff4930d9940799d6ec9ed3ffb6a4cfc052484
swordarrow2 <2856986197@qq.com>
2019-10-20 08:44:19 +0800

    ..

--- eclipse/src/main/java/com/meng/WarnMessageProcessor.java
+++ eclipse/src/main/java/com/meng/WarnMessageProcessor.java
@@ -52,7 +52,7 @@ public class WarnMessageProcessor {
         boolean b = false;
         if (lastMsg.equals(msg) && (msg.contains("~") || msg.contains("～"))) {
             b = processRepeat(fromGroup, fromQQ, msg);
-        } else if ((msg.contains("~") || msg.contains("～")) && isAtme(msg) && (!msg.contains("~转账") && !msg.contains("～转账")){
+        } else if ((msg.contains("~") || msg.contains("～")) && isAtme(msg) && (!msg.contains("~转账") && !msg.contains("～转账"))){
             onMsgHighWarinig(fromGroup, fromQQ);
             return true;
         }
