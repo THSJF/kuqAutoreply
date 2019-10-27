8530407cf504251d4458ded22c4d07141612cc30
swordarrow2 <2856986197@qq.com>
2019-10-27 13:59:36 +0800

    头像检测

--- eclipse/src/main/java/com/meng/picEdit/ThreeManager.java
+++ eclipse/src/main/java/com/meng/picEdit/ThreeManager.java
@@ -8,6 +8,7 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.URL;
 import java.util.*;
+import com.meng.config.javabeans.*;
 
 public class ThreeManager {
 
@@ -16,6 +17,9 @@ public class ThreeManager {
 
     public ThreeManager() {
 		checkSet.add(2487765013L);
+		checkSet.add(1033317031L);
+		checkSet.addAll(Autoreply.instence.configManager.configJavaBean.adminList);
+		checkSet.addAll(Autoreply.instence.configManager.configJavaBean.masterList);
 
         Autoreply.instence.threadPool.execute(new Runnable(){
 
@@ -29,7 +33,7 @@ public class ThreeManager {
 							} catch (InterruptedException e) {}
 						}
 						try {
-							Thread.sleep(300000);
+							Thread.sleep(60000);
 						} catch (InterruptedException e) {
 
 						}
@@ -48,6 +52,7 @@ public class ThreeManager {
         }
 		File headImageFile = new File(Autoreply.appDirectory + "user\\" + qq + ".jpg");
 		if (!headImageFile.exists()) {
+			downloadHead(qq);
 			changeMap.put(qq, false);
 			return;
 		}
@@ -64,7 +69,14 @@ public class ThreeManager {
 			return;
         }
 		changeMap.put(qq, true);
-
+		PersonInfo pi=Autoreply.instence.configManager.getPersonInfoFromQQ(qq);
+		String name;
+		if (pi == null) {
+			name = qq + "";
+		} else {
+			name = pi.name;
+		}
+		Autoreply.sendMessage(Autoreply.mainGroup,0,name + "更换了头像");
 	}
 
 	public boolean check(long fromGroup, long fromQQ) {
@@ -75,8 +87,34 @@ public class ThreeManager {
 		}
 		return false;
 	}
+
+	private void downloadHead(long qq) {
+        URL url;
+        try {
+            url = new URL("http://q2.qlogo.cn/headimg_dl?bs=" + qq + "&dst_uin=" + qq + "&dst_uin=" + qq + "&;dst_uin=" + qq + "&spec=5&url_enc=0&referer=bu_interface&term_type=PC");
+            DataInputStream dataInputStream = new DataInputStream(url.openStream());
+            FileOutputStream fileOutputStream = new FileOutputStream(headImageFile);
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = dataInputStream.read(buffer)) > 0) {
+                output.write(buffer, 0, length);
+            }
+            fileOutputStream.write(output.toByteArray());
+            dataInputStream.close();
+            fileOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }}
+
 	public void debug() {
-		changeMap.put(2856986197L, true);
-		Autoreply.sendMessage(Autoreply.mainGroup,0,"ready");
+		//changeMap.put(2856986197L, true);
+		//Autoreply.sendMessage(Autoreply.mainGroup, 0, "ready");
+		for (long qq:checkSet) {
+			check(qq);
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {}
+		}
 	}
 }
