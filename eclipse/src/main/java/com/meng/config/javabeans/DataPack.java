bbdd0c95af86cb711a88b75083a778c2fae56275
swordarrow2 <2856986197@qq.com>
2019-11-03 13:57:48 +0800

    修改为使用json

--- eclipse/src/main/java/com/meng/config/javabeans/DataPack.java
+++ eclipse/src/main/java/com/meng/config/javabeans/DataPack.java
@@ -2,44 +2,54 @@ package com.meng.config.javabeans;
 
 import java.io.*;
 import java.util.*;
+import com.google.gson.*;
+import com.meng.*;
 
 public class DataPack {
 
 	private byte[] data;
-	private int writePointer=0;
 	private final short headLength=18;
-	private int readPointer;
-
+	private JsonObject jsonObject;
+	private Gson gson;
+	private int writePointer=0;
+	private int longPointer=1;
+	private int stringPointer=1;
 	/*
 	 数据包中所有数字都是long型，除了数据包头中的数字和标记字符串长度的数字
 	 数据包中字符串的放置方式是先放置一个字符串在数据包中的字节数，数字为int，后面接着是字符串的数组，所有字符串都是utf-8
 	 数据包结构 : | 数据包头 | 数据 |
 	 数据包头 : | 包长度(4字节) | 头长度(2) | 数字"1"(2) | 时间戳或者任务标记(8) | 操作类型(2)|
-	 数据中的结构见下面操作类型的注释
+	 数据为json字符串,key见下面操作类型的注释
 
 	 小律影→正邪  小律影发送 鬼人正邪接收
 	 正邪→小律影  鬼人正邪发送 小律影接收
 	 小律影↔正邪  都可以发送和接收
+
+	 大部分情况下返回jsonObject
+	 jsonObject中字符串第一个叫s1 第二个叫s2 以此类推
+	 数字则是n1 n2.....
+	 {s1="此生无悔绀珠传",s2="来世愿打星莲船",n1=9961}
 	 */
-	public static final short _0notification=0;				//小律影↔正邪    |包头|字符串|   以下注释中省略"包头"说明
-	public static final short _1verify=1;					//小律影→正邪    |qq号(setConnect中设置的qq号)|
+
+	public static final short _0notification=0;				//小律影↔正邪    s1:通知文本
+	public static final short _1verify=1;					//小律影→正邪    n1:qq号(setConnect中设置的qq号)
 	public static final short _2getLiveList=2;				//小律影→正邪    不需要body
-	public static final short _3returnLiveList=3;			//正邪→小律影    |包头|json字符串|  例:[{"name":"闲者","qq":"877247145","bid":12007285,"bliveRoom":1954885,"tipIn":[],"tip":[true,true,false]},{"name":"懒瘦","qq":"496276037","bid":15272850,"bliveRoom":3144622,"tipIn":[],"tip":[true,true,false]}]
-	public static final short _4liveStart=4;				//正邪→小律影    |直播间号|主播称呼|
-	public static final short _5liveStop=5;					//正邪→小律影    |直播间号|主播称呼|
-	public static final short _6speakInLiveRoom=6;			//正邪→小律影    |直播间号|主播称呼|说话者称呼,如果配置文件中没有就是用户名|说话者BID|说话内容
-	public static final short _7newVideo=7;					//正邪→小律影    |用户名|视频名|AV号
-	public static final short _8newArtical=8;				//正邪→小律影    |用户名|专栏名|CV号
-	public static final short _9getPersonInfoByName=9;		//小律影→正邪    |称呼|
-	public static final short _10getPersonInfoByQQ=10;		//小律影→正邪    |qq号|
-	public static final short _11getPersonInfoByBid=11;		//小律影→正邪    |BID|
-	public static final short _12getPersonInfoByBiliLive=12;//小律影→正邪    |直播间号|
-	public static final short _13returnPersonInfo=13;		//正邪→小律影    |json字符串|    例:[{"name":"闲者","qq":"877247145","bid":12007285,"bliveRoom":1954885,"tipIn":[],"tip":[true,true,false]},{"name":"懒瘦","qq":"496276037","bid":15272850,"bliveRoom":3144622,"tipIn":[],"tip":[true,true,false]}]
-	public static final short _14coinsExchange=14;			//正邪→小律影    |幻币数量|
-	public static final short _15groupBan=15; 				//小律影↔正邪    |群号|QQ号|时间(秒)
-	public static final short _16groupKick=16;				//小律影↔正邪    |群号|QQ号|是否永久拒绝 0为否 1为是
-	public static final short _17heartBeat=17;				//心跳，不需要body
-	
+	public static final short _3returnLiveList=3;			//正邪→小律影    json数组  例:[{"name":"闲者","qq":"877247145","bid":12007285,"bliveRoom":1954885,"tipIn":[],"tip":[true,true,false]},{"name":"懒瘦","qq":"496276037","bid":15272850,"bliveRoom":3144622,"tipIn":[],"tip":[true,true,false]}]
+	public static final short _4liveStart=4;				//正邪→小律影    n1:直播间号 s1:主播称呼
+	public static final short _5liveStop=5;					//正邪→小律影    n1:直播间号 s1:主播称呼
+	public static final short _6speakInLiveRoom=6;			//正邪→小律影    n1:直播间号 s1:主播称呼 n2:说话者BID s2:说话者称呼,如果配置文件中没有就是用户名 s3:说话内容
+	public static final short _7newVideo=7;					//正邪→小律影    s1:用户名 s2:视频名 n1:AV号
+	public static final short _8newArtical=8;				//正邪→小律影    s1:用户名 s2:专栏名 n1:CV号
+	public static final short _9getPersonInfoByName=9;		//小律影→正邪    s1:称呼
+	public static final short _10getPersonInfoByQQ=10;		//小律影→正邪    n1:qq号
+	public static final short _11getPersonInfoByBid=11;		//小律影→正邪    n1:BID
+	public static final short _12getPersonInfoByBiliLive=12;//小律影→正邪    n1:直播间号
+	public static final short _13returnPersonInfo=13;		//正邪→小律影    json数组  例:[{"name":"闲者","qq":"877247145","bid":12007285,"bliveRoom":1954885,"tipIn":[],"tip":[true,true,false]},{"name":"懒瘦","qq":"496276037","bid":15272850,"bliveRoom":3144622,"tipIn":[],"tip":[true,true,false]}]
+	public static final short _14coinsAdd=14;				//正邪→小律影    n1:幻币数量 n2目标qq号
+	public static final short _15groupBan=15; 				//小律影↔正邪    n1:群号 n2:QQ号 n3:时间(秒)
+	public static final short _16groupKick=16;				//小律影↔正邪    n1:群号 n2:QQ号 n3:是否永久拒绝 0为否 1为是
+	public static final short _17heartBeat=17;				//小律影→正邪    心跳，不需要body
+
 	public static DataPack encode(short opCode, long timeStamp) {
 		return new DataPack(opCode, timeStamp);
 	}
@@ -49,25 +59,41 @@ public class DataPack {
 	}
 
 	private DataPack(short opCode, long timeStamp) {
-		data = new byte[1024];
+		gson = Autoreply.gson;
+		data = new byte[headLength];
 		write(getBytes(data.length));
 		write(getBytes(headLength));
 		write(getBytes((short)1));
 		write(getBytes(timeStamp));
 		write(getBytes(opCode));
-		//write(toEncode);
 	}   
 
 	private DataPack(byte[] pack) {
-		readPointer = headLength;
+		gson = Autoreply.gson;
 		data = pack;
+		jsonObject = gson.fromJson(new String(pack, headLength, getLength() - headLength), JsonObject.class);
 	} 
 
 	public byte[] getData() {
-		byte[] retData=new byte[readPointer];
-		for (int i=0;i < readPointer;++i) {
+		byte[] retData=new byte[headLength + gson.toJson(jsonObject).length()];
+		for (int i=0;i < headLength;++i) {
 			retData[i] = data[i];
 		}
+		byte[] bs=null;
+		try {
+			bs = gson.toJson(jsonObject).getBytes("utf-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return null;
+		}
+		for (int i=headLength;i < bs.length;++i) {
+			retData[i] = bs[i];
+		}
+		byte[] len=getBytes(retData.length);
+		retData[0] = len[0];
+		retData[1] = len[1];
+		retData[2] = len[2];
+		retData[3] = len[3];
 		return retData;
 	}
 
@@ -92,47 +118,25 @@ public class DataPack {
 	}
 
 	public void write(long l) {
-		write(getBytes(l));
+		jsonObject.addProperty("n" + longPointer, l);
 	}
 
 	public void write(String s) {
-		try {
-			write(s.getBytes("utf-8"));
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			write("UnsupportedEncodingException".getBytes());
-		}
+		jsonObject.addProperty("s" + stringPointer, s);
 	}
 
 	public long readNum() {
-		long l= readLong(data, readPointer);
-		readPointer += 8;
-		return l;
+		return jsonObject.get("n" + longPointer).getAsLong();
 	}
 
 	public String readString() {
-		int length=readInt(data, readPointer);
-		readPointer += 4;
-		String s=null;
-		try {                
-			s = new String(data, readPointer, length, "utf-8");
-		} catch (UnsupportedEncodingException e) {}
-		readPointer += length;
-		return s;
+		return jsonObject.get("s" + stringPointer).getAsString();
 	}
 
 	private void write(byte[] bs) {
-		if (data.length - readPointer - 1 < bs.length) {
-			byte[] newData=new byte[(data.length << 1) + bs.length];
-			for (int i=0;i < readPointer;++i) {
-				newData[i] = data[i];
-			}
-			data = newData;
-		}
 		for (int i=0;i < bs.length;++i) {
 			data[writePointer++] = bs[i];
 		}
-		readPointer += bs.length;
 	}
 
 	private byte[] getBytes(int i) {
