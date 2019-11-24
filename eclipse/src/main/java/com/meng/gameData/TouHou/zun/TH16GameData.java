4de9c0d47b78956b37c58d5a2cdbed3244056db3
swordarrow2 <2856986197@qq.com>
2019-11-24 17:24:38 +0800

    添加TH16游戏数据

---
+++ eclipse/src/main/java/com/meng/gameData/TouHou/zun/TH16GameData.java
@@ -0,0 +1,27 @@
+package com.meng.gameData.TouHou.zun;
+
+public class TH16GameData {
+	public static final String gameName="东方天空璋 ~ Hidden Star in Four Seasons.";
+	public static final String gameNameCN="东方天空璋";
+	public static final String gameNameAbbr="HSiFS";
+	public static String[] musicName=new String[]{
+		"樱花舞落的天空",
+		"希望之星直升青霄",
+		"仲夏的妖精梦",
+		"妖怪山间无色风",
+		"深山的遭遇",
+		"徜徉于樱色之海",
+		"一对的神兽",
+		"幻想的White Traveler",
+		"魔法的笠地藏",
+		"禁断之门对面，是此世还是彼世",
+		"Crazy Back Dancers",
+		"Into Backdoor",
+		"被秘匿的四个季节",
+		"门再也进不去了",
+		"秘神摩多罗 ~ Hidden Star in All Seasons.",
+		"不自然的自然",
+		"白色旅人",
+		"Player's Score"	
+	};
+}
