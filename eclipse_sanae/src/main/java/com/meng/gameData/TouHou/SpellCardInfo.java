34d5685a31d0d828792ea997c173ece9066cdae5
swordarrow2 <2856986197@qq.com>
2019-12-15 13:47:19 +0800

    补全一些信息

--- eclipse_sanae/src/main/java/com/meng/gameData/TouHou/SpellCardInfo.java
+++ eclipse_sanae/src/main/java/com/meng/gameData/TouHou/SpellCardInfo.java
@@ -7,185 +7,162 @@ public class SpellCardInfo {
 		sc = spc;
 		switch (sc.n) {
 			case "凶弹「高速撞击」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "弹符「射鹰」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "弹符「鹰已击中」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "铳符「月狂之枪」":
-				ps = "暂无";
+				ps = "东 方 跟 着 转";
 				break;
 			case "兔符「草莓团子」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "兔符「浆果浆果团子」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "兔符「团子影响力」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "月见「九月的满月」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "月见酒「月狂的九月」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "梦符「绯红色的噩梦」":
-				ps = "暂无";
-				break;
 			case "梦符「绯红色的压迫噩梦」":
-				ps = "暂无";
-				break;
 			case "梦符「蔚蓝色的愁梦」":
-				ps = "暂无";
-				break;
 			case "梦符「蔚蓝色的愁三重梦」":
-				ps = "暂无";
-				break;
 			case "梦符「愁永远之梦」":
-				ps = "暂无";
-				break;
 			case "梦符「刈安色的迷梦」":
-				ps = "暂无";
-				break;
 			case "梦符「刈安色的错综迷梦」":
-				ps = "暂无";
+				ps = "东 方 跟 着 转";
 				break;
 			case "梦符「捕梦网」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "梦符「苍蓝色的捕梦网」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "梦符「梦我梦中」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "月符「绀色的狂梦」":
-				ps = "暂无";
+				ps = "简单(?)的自机相关";
 				break;
 			case "玉符「乌合之咒」":
-				ps = "暂无";
-				break;
 			case "玉符「乌合的逆咒」":
-				ps = "暂无";
-				break;
 			case "玉符「乌合的二重咒」":
-				ps = "暂无";
+				ps = "简单(?)的狙";
 				break;
 			case "玉符「秽身探知型水雷」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "玉符「秽身探知型水雷 改」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "玉符「众神的弹冠」":
-				ps = "暂无";
-				break;
 			case "玉符「众神的光辉弹冠」":
-				ps = "暂无";
+				ps = "绀珠传最简单符卡（真）";
 				break;
 			case "「孤翼的白鹭」":
-				ps = "暂无";
+				ps = "多动症shadiao";
 				break;
 			case "狱符「地狱日食」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "狱符「地狱之蚀」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "狱符「闪光与条纹」":
-				ps = "暂无";
+				ps = "太野蛮了";
 				break;
 			case "狱符「星与条纹」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "狱炎「擦弹地狱火」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "狱炎「擦弹的狱意」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "地狱「条纹状的深渊」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "「伪阿波罗」":
-				ps = "暂无";
-				break;
 			case "「阿波罗捏造说」":
-				ps = "暂无";
+				ps = "简单(?)的固定弹";
 				break;
 			case "「掌上的纯光」":
-				ps = "暂无";
+				ps = "简单(真)的角随固";
 				break;
 			case "「杀意的百合」":
-				ps = "暂无";
-				break;
-			case "「现代的神灵界」":
-				ps = "暂无";
+				ps = "东 方 初 见 杀";
 				break;
 			case "「原始的神灵界」":
-				ps = "暂无";
+			case "「现代的神灵界」":
+				ps = "简单(?)的随机";
 				break;
 			case "「战栗的寒冷之星」":
-				ps = "暂无";
+				ps = "简单(?)的左右左右";
 				break;
 			case "「纯粹的疯狂」":
-				ps = "暂无";
+				ps = "简单(?)的角随固";
 				break;
 			case "「溢出的暇秽」":
-				ps = "暂无";
-				break;
 			case "「地上秽的纯化」":
-				ps = "暂无";
+				ps = "简单(?)的狙";
 				break;
 			case "纯符「单纯的子弹地狱」":
-				ps = "暂无";
-				break;
 			case "纯符「纯粹的弹幕地狱」":
-				ps = "p1:这tm是终符?你在逗我 p2:这tm是终符? p3:这tm... p4:这...";
+				ps = "简单(bu)的随机加固定\np1:这tm是终符?你在逗我\np2:这tm是终符?\np3:这tm...\np4:这...";
 				break;
 			case "蝴蝶「取而代之的蝴蝶」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "超特急「梦幻快车」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "爬梦「爬行的子弹」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "异界「逢魔之刻」":
-				ps = "暂无";
+				ps = "瞎了";
 				break;
 			case "地球「邪秽在身」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "月「阿波罗反射镜」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "「用于杀人的纯粹弹幕」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "异界「地狱的非理想弹幕」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "地球「落向地狱的雨」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "「用于逼死瓮中鼠的单纯弹幕」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "月「月狂冲击」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "「三位一体论狂想曲」":
-				ps = "暂无";
+				ps = "未填坑";
 				break;
 			case "「最初与最后的无名弹幕」":
-				ps = "暂无";
-				break;		
+				ps = "野蛮的二打一";
+				break;
+			default:
+				ps = "未填坑";
+				break;
 		}
 	}
 
