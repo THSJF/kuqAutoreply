99e17ee7e6bc7a6b2497a30c32b20033207f2b6e
c59862143 <8255053@qq.com>
2019-12-27 17:39:15 +0800

    留言处理

--- eclipse_sanae/src/main/java/com/meng/messageProcess/AdminMessageProcessor.java
+++ eclipse_sanae/src/main/java/com/meng/messageProcess/AdminMessageProcessor.java
@@ -53,13 +53,20 @@ public class AdminMessageProcessor {
 				SanaeConfigJavaBean.ReportBean rb = ConfigManager.ins.getReport();
 				Autoreply.ins.faithManager.addFaith(rb.q, 5);
 				ConfigManager.ins.removeReport();
-				MessageWaitManager.ins.addTip(rb.q, String.format("%d在%s的留言%s已经处理,获得5信仰奖励", rb.q, Tools.CQ.getTime(rb.t), rb.c));
-				Autoreply.sendMessage(fromGroup,0,"处理成功");
+				MessageWaitManager.ins.addTip(rb.q, String.format("%d在%s的留言「%s」已经处理,获得5信仰奖励", rb.q, Tools.CQ.getTime(rb.t), rb.c));
+				Autoreply.sendMessage(fromGroup, 0, "处理成功");
 				return true;
 			}
 			if (msg.equals("-留言查看 f")) {
 				ConfigManager.ins.removeReport();
-				Autoreply.sendMessage(fromGroup,0,"处理成功");
+				Autoreply.sendMessage(fromGroup, 0, "处理成功");
+				return true;
+			}
+			if (msg.equalsIgnoreCase("-留言查看 w")) {
+				SanaeConfigJavaBean.ReportBean rb = ConfigManager.ins.getReport();
+				ConfigManager.ins.reportToLast();
+				MessageWaitManager.ins.addTip(rb.q, String.format("%d在%s的留言「%s」已经处理,开发者认为目前还不是处理此留言的时候", rb.q, Tools.CQ.getTime(rb.t), rb.c));
+				Autoreply.sendMessage(fromGroup, 0, "处理成功");
 				return true;
 			}
 			if (msg.equals("-反馈查看")) {
@@ -70,13 +77,20 @@ public class AdminMessageProcessor {
 				SanaeConfigJavaBean.BugReportBean brb = ConfigManager.ins.getBugReport();
 				Autoreply.ins.faithManager.addFaith(brb.q, 10);
 				ConfigManager.ins.removeBugReport();
-				MessageWaitManager.ins.addTip(brb.q, String.format("%d在%s的反馈%s已经处理,获得10信仰奖励", brb.q, Tools.CQ.getTime(brb.t), brb.c));
-				Autoreply.sendMessage(fromGroup,0,"处理成功");
+				MessageWaitManager.ins.addTip(brb.q, String.format("%d在%s的反馈「%s」已经处理,获得10信仰奖励", brb.q, Tools.CQ.getTime(brb.t), brb.c));
+				Autoreply.sendMessage(fromGroup, 0, "处理成功");
 				return true;
 			}
 			if (msg.equals("-反馈查看 f")) {
 				ConfigManager.ins.removeBugReport();
-				Autoreply.sendMessage(fromGroup,0,"处理成功");
+				Autoreply.sendMessage(fromGroup, 0, "处理成功");
+				return true;
+			}
+			if (msg.equalsIgnoreCase("-反馈查看 w")) {
+				SanaeConfigJavaBean.BugReportBean brb = ConfigManager.ins.getBugReport();
+				ConfigManager.ins.bugReportToLast();
+				MessageWaitManager.ins.addTip(brb.q, String.format("%d在%s的反馈「%s」已经处理,开发者认为暂时不需要处理此问题", brb.q, Tools.CQ.getTime(brb.t), brb.c));
+				Autoreply.sendMessage(fromGroup, 0, "处理成功");
 				return true;
 			}
 			if (msg.startsWith("群广播:")) {
